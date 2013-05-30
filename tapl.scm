55c82c33763251b9434ae384610737d37fde6f4a
Nada Amin <namin@alum.mit.edu>
2013-05-30 22:21:37 +0200

    cartesiano... slow and dup galore. cc @webyrd

--- tapl.scm
+++ tapl.scm
@@ -16,9 +16,38 @@
 
 (test-check
  "map-seto"
- ;; running with 1, because we get dups :-(
  (run 1 (q) (map-seto (lambda (x out) (== `(,x) out))
                       (set ∅ 'a 'b 'c)
                       q))
  '((set ∅ (a) (b) (c))))
+
+(test-check
+ "map-seto-converges"
+ (length
+  (run* (q) (map-seto (lambda (x out) (== `(,x) out))
+                      (set ∅ 'a 'b 'c)
+                      q)))
+ 6)
+
+(define cartesiano
+  (lambda (a b out)
+    (conde
+     ((== a ∅)
+      (== out ∅))
+     ((fresh (ae ar o1 or)
+        (== a (set ar ae))
+        (!ino ae ar)
+        (map-seto (lambda (be oe) (== oe `(,ae ,be))) b o1)
+        (uniono o1 or out)
+        (cartesiano ar b or))))))
+
+(test-check
+ "cartesiano"
+ (run 1 (q) (cartesiano (set ∅ 'a 'b 'c) (set ∅ 1 2 3) q))
+ '((set ∅ (a 1) (a 2) (a 3) (b 1) (b 2) (b 3) (c 1) (c 2) (c 3))))
+
+(test-check
+ "cartesiano-converges"
+ (length (run* (q) (cartesiano (set ∅ 'a 'b) (set ∅ 1 2) q)))
+ 128)
 
